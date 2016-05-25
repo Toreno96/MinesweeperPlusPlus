cf232a8b7fbce24d32f0c1725116861448b02e80
Toreno96 <toreno96@gmail.com>
2016-05-26 00:41:32 +0200

    commit -m "MinefieldHex::findSurroundingCellsColumnsBounds implemented"

--- src/GameLogic/MinefieldHex.cpp
+++ src/GameLogic/MinefieldHex.cpp
@@ -4,22 +4,19 @@
 MinefieldHex::MinefieldHex( const CellsVector2D &cells ) : MinefieldNonclassic( 7, cells ) {}
 MinefieldHex::MinefieldHex( const std::size_t rowsCount, const std::size_t columnsCount, const std::size_t minesCount ) : MinefieldHex( CellsVector2D( rowsCount, columnsCount, minesCount ) ) {}
 // Pomocnicze metody chronione:
-MinefieldHex::tColumnsBoundsSet MinefieldHex::findSurroundingCellsColumnsBounds( const std::size_t centerRow, const std::size_t centerColumn ) const { // TO-DO
+MinefieldHex::tColumnsBoundsSet MinefieldHex::findSurroundingCellsColumnsBounds( const std::size_t centerRow, const std::size_t centerColumn ) const {
   std::size_t minColumnTop, maxColumnTop,
-              minColumnMiddle = findLowerCoordinate( centerColumn, 0, 2 ),
-              maxColumnMiddle = findGreaterCoordinate( centerColumn, mRowsCount - 1, 2 ),
-              minColumnBottom, maxColumnBottom;
-  if( centerRow % 2 == centerColumn % 2 ) {
+              minColumnMiddle = findLowerCoordinate( centerColumn ),
+              maxColumnMiddle = findGreaterCoordinate( centerColumn, mColumnsCount - 1 ),
+              &minColumnBottom = minColumnTop,
+              &maxColumnBottom = maxColumnTop;
+  if( centerRow % 2 == 0 ) {
     minColumnTop = findLowerCoordinate( centerColumn );
-    maxColumnTop = findGreaterCoordinate( centerColumn, mRowsCount - 1 );
-    minColumnBottom = minColumnMiddle;
-    maxColumnBottom = maxColumnMiddle;
+    maxColumnTop = centerColumn;
   }
   else {
-    minColumnTop = minColumnMiddle;
-    maxColumnTop = maxColumnMiddle;
-    minColumnBottom = findLowerCoordinate( centerColumn );
-    maxColumnBottom = findGreaterCoordinate( centerColumn, mRowsCount - 1 );
+    minColumnTop = centerColumn;
+    maxColumnTop = findGreaterCoordinate( centerColumn, mColumnsCount - 1 );
   }
   return { { { { minColumnTop, maxColumnTop } },
              { { minColumnMiddle, maxColumnMiddle } },
