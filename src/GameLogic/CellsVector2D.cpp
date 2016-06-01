dc750e94e14cf1bd567a8ab3e1354a1cc1aefef0
Toreno96 <toreno96@gmail.com>
2016-06-01 20:15:58 +0200

    Changed CellsVector2D to MinefieldData; mUsedFlags to mUsedFlagsCount; added mElapsedSeconds

--- src/GameLogic/CellsVector2D.cpp
+++ src/GameLogic/CellsVector2D.cpp
@@ -1,33 +1,42 @@
-#include "CellsVector2D.hpp"
+#include "MinefieldData.hpp"
 
 // Konstruktory:
-CellsVector2D::CellsVector2D() : CellsVector2D( 0, 0, 0 ) {}
-CellsVector2D::CellsVector2D( const std::size_t rowsCount, const std::size_t columnsCount, const std::size_t minesCount, std::size_t uncoveredCellsCount, std::size_t usedFlags, bool mined ) : mRowsCount( rowsCount ), mColumnsCount( columnsCount ), mMinesCount( minesCount ), mUncoveredCellsCount( uncoveredCellsCount ), mUsedFlags( usedFlags ), mCells( std::vector< std::vector< Cell > >( rowsCount ) ), mMined( mined ) {
+MinefieldData::MinefieldData() : MinefieldData( 0, 0, 0 ) {}
+MinefieldData::MinefieldData( const std::size_t rowsCount, const std::size_t columnsCount, const std::size_t minesCount, std::size_t uncoveredCellsCount, unsigned elapsedSeconds, std::size_t usedFlagsCount, bool mined ) : mRowsCount( rowsCount ), mColumnsCount( columnsCount ), mMinesCount( minesCount ), mUncoveredCellsCount( uncoveredCellsCount ), mUsedFlagsCount( usedFlagsCount ), mElapsedSeconds( elapsedSeconds ), mCells( std::vector< std::vector< Cell > >( rowsCount ) ), mMined( mined ) {
   for( auto &row : mCells )
     row = std::vector< Cell >( columnsCount );
 }
 // Gettery:
-std::size_t CellsVector2D::getRowsCount() const {
+std::size_t MinefieldData::getRowsCount() const {
   return mRowsCount;
 }
-std::size_t CellsVector2D::getColumnsCount() const {
+std::size_t MinefieldData::getColumnsCount() const {
   return mColumnsCount;
 }
-std::size_t CellsVector2D::getMinesCount() const {
+std::size_t MinefieldData::getMinesCount() const {
   return mMinesCount;
 }
-std::size_t CellsVector2D::getUncoveredCellsCount() const {
+std::size_t MinefieldData::getUncoveredCellsCount() const {
   return mUncoveredCellsCount;
 }
-std::size_t CellsVector2D::getUsedFlags() const {
-  return mUsedFlags;
+std::size_t MinefieldData::getUsedFlagsCount() const {
+  return mUsedFlagsCount;
 }
-Cell &CellsVector2D::getCell( const std::size_t row, const std::size_t column ) {
+unsigned MinefieldData::getElapsedSeconds() const {
+  return mElapsedSeconds;
+}
+/*
+Cell &MinefieldData::getCell( const std::size_t row, const std::size_t column ) {
   return mCells[ row ][ column ];
 }
-const Cell &CellsVector2D::getCell( const std::size_t row, const std::size_t column ) const {
+*/
+const Cell &MinefieldData::getCell( const std::size_t row, const std::size_t column ) const {
   return mCells[ row ][ column ];
 }
-bool CellsVector2D::isMined() const {
+bool MinefieldData::isMined() const {
   return mMined;
+}
+// Settery:
+void MinefieldData::setElapsedSeconds( unsigned elapsedSeconds ) {
+  mElapsedSeconds = elapsedSeconds;
 }
--- src/GameLogic/CellsVector2D.hpp
+++ src/GameLogic/CellsVector2D.hpp
@@ -3,23 +3,27 @@
 #include <vector>
 #include "Cell.hpp"
 
-class CellsVector2D {
+class MinefieldData {
   public:
     // Konstruktory:
-    CellsVector2D();
-    CellsVector2D( const std::size_t rowsCount, const std::size_t columnsCount, const std::size_t minesCount, std::size_t uncoveredCellsCount = 0, std::size_t usedFlags = 0, bool mined = false );
+    MinefieldData();
+    MinefieldData( const std::size_t rowsCount, const std::size_t columnsCount, const std::size_t minesCount, std::size_t uncoveredCellsCount = 0, std::size_t usedFlagsCount = 0, unsigned elapsedSeconds = 0, bool mined = false );
     // Gettery:
     std::size_t getRowsCount() const;
     std::size_t getColumnsCount() const;
     std::size_t getMinesCount() const;
     std::size_t getUncoveredCellsCount() const;
-    std::size_t getUsedFlags() const;
-    Cell &getCell( const std::size_t row, const std::size_t column );
+    std::size_t getUsedFlagsCount() const;
+    unsigned getElapsedSeconds() const;
+    //Cell &getCell( const std::size_t row, const std::size_t column );
     const Cell &getCell( const std::size_t row, const std::size_t column ) const;
     bool isMined() const;
+    // Settery:
+    void setElapsedSeconds( unsigned elapsedSeconds );
   protected:
     // Składowe:
-    std::size_t mRowsCount, mColumnsCount, mMinesCount, mUncoveredCellsCount, mUsedFlags;
+    std::size_t mRowsCount, mColumnsCount, mMinesCount, mUncoveredCellsCount, mUsedFlagsCount;
+    unsigned mElapsedSeconds;
     std::vector< std::vector< Cell > > mCells;
     bool mMined;
 };
