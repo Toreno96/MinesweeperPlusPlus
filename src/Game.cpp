e4007f0b4b35b0ffd3c453ef343bb2c8c991efd6
Toreno96 <toreno96@gmail.com>
2016-06-03 17:15:41 +0200

    Game (State Machine) class fully implemented

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,6 +1,15 @@
-#include "Game.hpp"
+#include "GameStates/MenuScreen.hpp"
 
-Game::Game() {}
+// Konstruktor:
+Game::Game() : mHighScores( HighScores() ), mOptions( Options() ), mSaveManager( SaveManager() ), mStates( std::stack< std::unique_ptr< GameState > >() ), mVideoMode( sf::VideoMode( 800, 600 ) ) {
+  pushState( std::make_unique< MenuScreen >( this ) );
+  mHighScores.loadFromFile();
+  mOptions.loadFromFile();
+  mSaveManager.loadFromFile();
+}
+// Destruktor:
+Game::~Game() {}
+// Obsługa stosu stanów:
 void Game::pushState( std::unique_ptr< GameState > state ) {
   mStates.push( std::move( state ) );
 }
@@ -12,10 +21,33 @@ void Game::changeState( std::unique_ptr< GameState > state ) {
   popState();
   pushState( std::move( state ) );
 }
-GameState* Game::topState() {
-  if( !( mStates.empty() ) )
-    return mStates.top().get();
-  else
+GameState *Game::topState() const {
+  if( mStates.empty() )
     return nullptr;
+  else
+    return mStates.top().get();
 }
-void Game::run() {}
+// Uruchomienie i zakończenie gry:
+void Game::run() {
+  createWindow( mWindow );
+  while( mWindow.isOpen() ) {
+    GameState *pTopState = topState();
+    if( pTopState == nullptr )
+      mWindow.close();
+    pTopState->handleInput();
+    pTopState->update();
+    mWindow.clear( sf::Color::Black );
+    pTopState->draw();
+    mWindow.display();
+  }
+}
+void Game::exit() {
+  mWindow.close();
+}
+// Pomocnicze metody chronione:
+void Game::createWindow( sf::RenderWindow &window ) {
+  sf::ContextSettings settings;
+  settings.antialiasingLevel = 8;
+  window.create( mVideoMode, GameConstants::title, sf::Style::Titlebar | sf::Style::Close, settings );
+  window.setFramerateLimit( 60 );
+}
--- src/Game.hpp
+++ src/Game.hpp
@@ -6,26 +6,36 @@
 #include "GameLogic/HighScores.hpp"
 #include "GameLogic/Options.hpp"
 #include "GameLogic/SaveManager.hpp"
-//#include "GameState.hpp"
+#include "HelperFunctions/GameConstants.hpp"
 
 class GameState;
 
 class Game {
   public:
+    // Konstruktor:
     Game();
+    // Destruktor:
+    ~Game();
+    // Usunięte metody domyślne:
     Game( const Game &other ) = delete;
     Game &operator=( Game other ) = delete;
-    
+    // Obsługa stosu stanów:
     void pushState( std::unique_ptr< GameState > state );
     void popState();
     void changeState( std::unique_ptr< GameState > state );
-    GameState* topState();
-    
+    GameState *topState() const;
+    // Uruchomienie i zakończenie gry:
     void run();
-  private:
-    std::stack< std::unique_ptr< GameState > > mStates;
-    sf::RenderWindow mWindow;
+    void exit();
+    // Składowe publiczne:
     HighScores mHighScores;
     Options mOptions;
     SaveManager mSaveManager;
+    sf::RenderWindow mWindow;
+  protected:
+    // Składowe chronione:
+    std::stack< std::unique_ptr< GameState > > mStates;
+    sf::VideoMode mVideoMode;
+    // Pomocnicze metody chronione:
+    void createWindow( sf::RenderWindow &window );
 };
