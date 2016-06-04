74a61e313208b116296a964cef14c517810b40fe
Toreno96 <toreno96@gmail.com>
2016-06-04 18:39:26 +0200

    Added colors and font name constants

--- src/HelperSrc/GameConstants.hpp
+++ src/HelperSrc/GameConstants.hpp
@@ -1,9 +1,17 @@
 #pragma once
 
 #include <string>
+#include <SFML/Graphics.hpp>
 
 namespace GameConstants {
   
   const std::string title = "Minesweeper++";
+  const std::string fontName = "times.tff";
+  // Colors RGB codes from http://www.rapidtables.com/web/color/RGB_Color.htm
+  const sf::Color defaultColor = sf::Color::White;
+  const sf::Color blue = sf::Color( 30, 144, 255 ); // Dodger Blue
+  const sf::Color lightBlue = sf::Color( 175, 238, 238 ); // Pale Turquoise
+  const sf::Color red = sf::Color( 235, 0, 0 ); // Custom Red
+ 
   
 } // namespace GameConstants
