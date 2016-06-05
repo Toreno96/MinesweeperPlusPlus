dbca7cf2d5da210f2de4d3c25665ef989cb79d88
Toreno96 <toreno96@gmail.com>
2016-06-05 15:51:42 +0200

    Added color for deactivated button

--- src/HelperSrc/GameConstants.hpp
+++ src/HelperSrc/GameConstants.hpp
@@ -9,6 +9,7 @@ namespace GameConstants {
   const std::string fontName = "times.ttf";
   // Colors RGB codes from http://www.rapidtables.com/web/color/RGB_Color.htm
   const sf::Color defaultColor = sf::Color::White;
+  const sf::Color deactivatedButton = sf::Color	( 192, 192, 192 ); // Silver
   const sf::Color blue = sf::Color( 30, 144, 255 ); // Dodger Blue
   const sf::Color lightBlue = sf::Color( 175, 238, 238 ); // Pale Turquoise
   const sf::Color red = sf::Color( 235, 0, 0 ); // Custom Red
